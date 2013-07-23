4fb6930793b6c5df27999ef6e383a454cae986e7
vforge <bartosz.bentkowski@gmail.com>
2013-07-23 02:37:54 +0200

    bugfix attempt 3

--- lib/basic-scopes.rb
+++ lib/basic-scopes.rb
@@ -7,36 +7,36 @@ module BasicScopes
   extend ActiveSupport::Concern
 
   included do
-    def except_ids(ids) do
+    def except_ids(ids)
       self.where("#{self.table_name}.id NOT IN (?)", ids)
     end
 
-    def filter_ids(ids) do
+    def filter_ids(ids)
       self.where("#{self.table_name}.id IN (?)", ids)
     end
 
-    def filter_last_updated(time) do
+    def filter_last_updated(time)
       self.where("#{self.table_name}.updated_at > ?", time.ago)
     end
 
 
-    def by_id do
+    def by_id
       self.order("#{self.table_name}.id DESC") }
     end
 
-    def by_id_reversed do
+    def by_id_reversed
       self.order("#{self.table_name}.id ASC") }
     end
 
-    def by_created_at do
+    def by_created_at
       self.order("#{self.table_name}.created_at DESC") }
     end
 
-    def by_created_at_reversed do
+    def by_created_at_reversed
       self.order("#{self.table_name}.created_at ASC") }
     end
 
-    def by_updated_at do
+    def by_updated_at
       self.order("#{self.table_name}.updated_at DESC") }
     end
 
