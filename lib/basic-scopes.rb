6cc242dd0018ebc5fd6d43900e1cb97e7dd12ae8
vforge <bartosz.bentkowski@gmail.com>
2013-07-23 02:44:04 +0200

    bugfix attempt 7

--- lib/basic-scopes.rb
+++ lib/basic-scopes.rb
@@ -7,43 +7,44 @@ module BasicScopes
   extend ActiveSupport::Concern
 
   included do
-    def except_ids(ids)
-      self.where("#{self.table_name}.id NOT IN (?)", ids)
+    class << self
+      def except_ids(ids)
+        self.where("#{self.table_name}.id NOT IN (?)", ids)
+      end
+
+      def filter_ids(ids)
+        self.where("#{self.table_name}.id IN (?)", ids)
+      end
+
+      def filter_last_updated(time)
+        self.where("#{self.table_name}.updated_at > ?", time.ago)
+      end
+
+
+      def by_id
+        self.order("#{self.table_name}.id DESC")
+      end
+
+      def by_id_reversed
+        self.order("#{self.table_name}.id ASC")
+      end
+
+      def by_created_at
+        self.order("#{self.table_name}.created_at DESC")
+      end
+
+      def by_created_at_reversed
+        self.order("#{self.table_name}.created_at ASC")
+      end
+
+      def by_updated_at
+        self.order("#{self.table_name}.updated_at DESC")
+      end
+
+      def by_updated_at_reversed
+        self.order("#{self.table_name}.updated_at ASC")
+      end
     end
-
-    def filter_ids(ids)
-      self.where("#{self.table_name}.id IN (?)", ids)
-    end
-
-    def filter_last_updated(time)
-      self.where("#{self.table_name}.updated_at > ?", time.ago)
-    end
-
-
-    def by_id
-      self.order("#{self.table_name}.id DESC")
-    end
-
-    def by_id_reversed
-      self.order("#{self.table_name}.id ASC")
-    end
-
-    def by_created_at
-      self.order("#{self.table_name}.created_at DESC")
-    end
-
-    def by_created_at_reversed
-      self.order("#{self.table_name}.created_at ASC")
-    end
-
-    def by_updated_at
-      self.order("#{self.table_name}.updated_at DESC")
-    end
-
-    def by_updated_at_reversed
-      self.order("#{self.table_name}.updated_at ASC")
-    end
-
   end
 
   module ClassMethods
