91c561e3691cf496de0ceb6a0bd604d91b6bcd06
vforge <bartosz.bentkowski@gmail.com>
2013-07-23 02:25:55 +0200

    bugfix attempt 1

--- lib/basic-scopes.rb
+++ lib/basic-scopes.rb
@@ -6,17 +6,17 @@ require "active_support" unless defined?(ActiveSupport)
 module BasicScopes
   extend ActiveSupport::Concern
 
-  included do
-    scope :except_ids, ->(ids) { where("#{self.table_name}.id NOT IN (?)", ids) }
-    scope :filter_ids, ->(ids) { where("#{self.table_name}.id IN (?)", ids) }
-    scope :filter_last_updated, ->(time) { where("#{self.table_name}.updated_at > ?", time.ago) }
+  included do |klass|
+    scope :except_ids, ->(ids) { where("#{klass.table_name}.id NOT IN (?)", ids) }
+    scope :filter_ids, ->(ids) { where("#{klass.table_name}.id IN (?)", ids) }
+    scope :filter_last_updated, ->(time) { where("#{klass.table_name}.updated_at > ?", time.ago) }
 
-    scope :by_id,                   ->{ order("#{self.table_name}.id DESC") }
-    scope :by_id_reversed,          ->{ order("#{self.table_name}.id ASC") }
-    scope :by_created_at,           ->{ order("#{self.table_name}.created_at DESC") }
-    scope :by_created_at_reversed,  ->{ order("#{self.table_name}.created_at ASC") }
-    scope :by_updated_at,           ->{ order("#{self.table_name}.updated_at DESC") }
-    scope :by_updated_at_reversed,  ->{ order("#{self.table_name}.updated_at ASC") }
+    scope :by_id,                   ->{ order("#{klass.table_name}.id DESC") }
+    scope :by_id_reversed,          ->{ order("#{klass.table_name}.id ASC") }
+    scope :by_created_at,           ->{ order("#{klass.table_name}.created_at DESC") }
+    scope :by_created_at_reversed,  ->{ order("#{klass.table_name}.created_at ASC") }
+    scope :by_updated_at,           ->{ order("#{klass.table_name}.updated_at DESC") }
+    scope :by_updated_at_reversed,  ->{ order("#{klass.table_name}.updated_at ASC") }
   end
 
   module ClassMethods
